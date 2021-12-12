d2ca7ba6e4c3572a19b05ae9f353b3bc47937987
RozeQz <liza.roz@yandex.ru>
2021-12-12 16:43:23 +0300

    Initial commit

---
+++ db/migrate/20211212133434_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false, index: { unique: true }
+      t.string :email, null: false, index: { unique: true }
+      t.string :password_digest
+      t.datetime :last_login_at
+
+      t.timestamps
+    end
+  end
+end
