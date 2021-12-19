75389d14e108a85873874c7a38ee15eab9526fde
RozeQz <liza.roz@yandex.ru>
2021-12-19 19:42:27 +0300

    Add language button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,27 @@ class ApplicationController < ActionController::Base
                                              user_signed_in? &&
                                                (current_user.last_login_at.nil? || current_user.last_login_at < 3.minutes.ago)
                                            }
+  around_action :switch_locale
 
   def update_last_login_at
     current_user.update_attribute(:last_login_at, Time.current)
   end
 
   private
+
+  def switch_locale(&action)
+    locale = locale_from_url || I18n.default_locale
+    I18n.with_locale locale, &action
+  end
+
+  def locale_from_url
+    locale = params[:locale]
+    return locale if I18n.available_locales.map(&:to_s).include?(locale)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   def require_login
     redirect_to session_login_url unless signed_in?
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  root 'posts#index'
-  get 'session/login'
-  post 'session/create'
-  get 'session/logout'
-  resources :users do
-    member do
-      get "collection"
+  scope "(:locale)", locale: /#{I18n.available_locales.join('|')}/ do
+    root 'posts#index'
+    get 'session/login'
+    post 'session/create'
+    get 'session/logout'
+    resources :users do
+      member do
+        get "collection"
+      end
     end
-  end
-  resources :posts, except: %i[show] do
-    member do
-      patch "upvote", to: "posts#upvote"
-      get 'author_options', to: 'posts#author_options'
+    resources :posts, except: %i[show] do
+      member do
+        patch "upvote", to: "posts#upvote"
+        get 'author_options', to: 'posts#author_options'
+      end
     end
   end
 end
