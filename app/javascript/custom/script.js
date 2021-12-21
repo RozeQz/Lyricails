29b03e2c2dbe48d20ac9dddbdb4a127c0ce11bc9
RozeQz <liza.roz@yandex.ru>
2021-12-21 23:53:10 +0300

    Fix multiple delete

--- app/javascript/custom/script.js
+++ app/javascript/custom/script.js
@@ -1,7 +1,6 @@
 let last = {};
 document.addEventListener('DOMContentLoaded',() =>{
   document.querySelectorAll('audio').forEach(function(el){
-    console.log('forEach')
     el.addEventListener('playing', function(){
       if (last.pause && last !== this)
         last.pause()
